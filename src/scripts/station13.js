7baff6241ff09e222a9d707a8c3158b055b559d0
Takuma Nitta <takuma@TakumanoMacBook-Pro.local>
2021-08-08 16:26:18 +0900

    Update station13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,5 @@
-function getData() {
+export function getData() {
     // Arrowという文字列を返す処理をアロー関数を入れて書きましょう。
-    return // ここにアロー関数の処理を書く
+    let show =()=> 'Arrow';
+    return show()// ここにアロー関数の処理を書く
 }
